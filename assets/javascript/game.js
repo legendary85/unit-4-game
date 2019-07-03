960c2bb8822b8ca431e517e17237608c6ec7f8ac
Kourtney McCullough <kourtneyjmccullough@gmail.com>
2019-07-03 13:29:52 -0500

    built html  added css design

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+/*
+At start of game a randomly generated number (19-120) will be displayed .
+
+There will be 4 crystals will be assigned random numbers (1-12) that are hidden.
+
+When the crystals a clicked number will be displayed in a "your number box".
+
+As the crystals are clicked to corresponding number will be added the the previous
+selected number.
+
+If total is equal to the randomly generated number, user wins.
+
+Number of wins will be dispalyed in the wins area.
+
+Game resets and new numbers are randomly generated for both the 
+computer display and crystals.
+
+If total is not equal to the randomly generated number, user loses.
+
+Number of losses will be displayed in the losses area.
+
+Game resets and new numbers are randomly generated for both the 
+computer display and crystals.
+
+
+*/
